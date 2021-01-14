afcd59105390f0c793cc4638a260aa75781078ff
Kelsey L Grantham <kelsey.grantham@monash.edu>
2021-01-14 16:05:17 +1100

    Creates Latex code for generating results table for publication

--- results_tables.R
+++ results_tables.R
@@ -5,6 +5,9 @@
 #
 
 library(tidyverse)
+library(tables)
+library(knitr)
+library(kableExtra)
 
 file.names <- dir('./performance_measures')
 
@@ -25,6 +28,8 @@ for (i in 1:length(file.names)) {
     select(c(theta, WPICC, BPICC, method))
   biasrows <- cbind(params, biasvals)
   allbiasrows <- rbind(allbiasrows, biasrows)
+  # May need to format values with desired number of digits here
+  # as Format() in tables package doesn't seem to work
   
   # RMSE values
   RMSEvals <- measures %>%
@@ -59,3 +64,46 @@ RMSEdf <- allRMSErows %>%
     names_to='parameters',
     values_to='RMSE'
   )
+
+# Create formatted results table
+
+# Create dummy results dataframe (for now)
+dat <- expand.grid(
+  S = c(1,2,5),
+  Tp = c(5, 9),
+  m = c(10, 100),
+  rho1 = c(0.05, 0.1),
+  r = 0.8,
+  method = c('MCMC', 'REML')
+)
+dat$theta <- rnorm(dim(dat)[1])
+dat$WPICC <- rnorm(dim(dat)[1])
+dat$BPICC <- rnorm(dim(dat)[1])
+
+biasdf <- dat %>%
+  pivot_longer(
+    cols=c(theta, WPICC, BPICC),
+    names_to='parameters',
+    values_to='bias'
+  )
+
+# Create Latex code for generating table
+toLatex(
+  tabular(
+    RowFactor(rho1, spacing=1)
+    * RowFactor(Tp, spacing=1)
+    * RowFactor(m, spacing=1)
+    * Factor(S)
+    ~ Heading()*RowFactor(parameters)
+    * Heading()*RowFactor(method)
+    * Heading()*bias
+    * Heading()*identity,
+    data=biasdf
+  )
+)
+
+# kable and kableExtra don't seem to be able to generate
+# Latex code and can't seem to handle nested columns and rows as well
+kbl(biasdf, booktabs=T) %>%
+  kable_styling() %>%
+  collapse_rows(columns=1:4, valign="top")
