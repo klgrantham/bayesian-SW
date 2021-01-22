365e82f9e8975fee8369f2e11c68debd6fd84e0d
Kelsey L Grantham <kelsey.grantham@monash.edu>
2021-01-22 13:21:43 +1100

    Uses unique spec_id to get each configuration for processing results

--- run-measures.R
+++ run-measures.R
@@ -8,21 +8,21 @@ source('performance-measures.R')
 
 v <- Sys.getenv('SLURM_ARRAY_TASK_ID', NA)
 vn <- as.integer(v)
-row <- vn + 1
-print(sprintf('We will run job #%d',row))
-params <- read.csv('parameters.csv')
-myparams = params[row,]
+print(sprintf('We will run job #%d', vn))
+params <- read.csv('parameters_batched.csv')
+# Get unique specification with spec_id
+myparams <- params[params$spec_id==vn,][1,]
 print('We will use the params:')
 print(myparams)
 
-reduce_all_results(2, myparams$clust_per_seq, myparams$periods,
+reduce_all_results(10, myparams$clust_per_seq, myparams$periods,
                    myparams$subjects, myparams$WPICC,
                    myparams$CAC, myparams$theta)
 
-collate_results(2, myparams$clust_per_seq, myparams$periods,
+collate_results(10, myparams$clust_per_seq, myparams$periods,
                 myparams$subjects, myparams$WPICC,
                 myparams$CAC, myparams$theta)
 
-calculate_measures(2, myparams$clust_per_seq, myparams$periods,
+calculate_measures(myparams$clust_per_seq, myparams$periods,
                    myparams$subjects, myparams$WPICC,
                    myparams$CAC, myparams$theta)
