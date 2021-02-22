b14023aa65f950dcda1d34cdc2e1ead16d21cf71
Kelsey L Grantham <kelsey.grantham@monash.edu>
2021-02-22 15:06:43 +1100

    Generate plots of bias and MSE for CAC and relative % error in model-based SE for theta

--- paper-plots.R
+++ paper-plots.R
@@ -243,6 +243,48 @@ ggsave("plots/bias_BPICC.jpg", p_bias_BPICC, width=9, height=4, units="in", dpi=
 ggsave("plots/bias_BPICC.pdf", p_bias_BPICC, width=9, height=4, units="in", dpi=600)
 
 
+## Plot bias for CAC (r)
+# Note: Only for HG model, not HH
+
+# Determine common y-axis limits
+minbiasHG <- signif(
+  biasdfHG %>%
+    filter(parameters=='CAC') %>%
+    summarise(min(value)),
+  2)
+maxbiasHG <- signif(
+  biasdfHG %>%
+    filter(parameters=='CAC') %>%
+    summarise(max(value)),
+  2)
+ylims <- c(min(minbiasHG), max(maxbiasHG))
+
+# Get separate results blocks for plotting
+biasdf_WPICC5_CAC80_CAC <- biasdfHG %>%
+  filter(rho1==0.05 & r==0.8 & parameters=='CAC')
+biasdf_WPICC10_CAC80_CAC <- biasdfHG %>%
+  filter(rho1==0.1 & r==0.8 & parameters=='CAC')
+
+# Generate grid plots
+p1 <- plotminigrid(
+  biasdf_WPICC5_CAC80_CAC,
+  ylims=ylims,
+  title=expression(paste(rho[1], " = 0.05", ", r = 0.8"))
+) + geom_hline(yintercept=0, linetype='dashed')
+p2 <- plotminigrid(
+  biasdf_WPICC10_CAC80_CAC,
+  ylims=ylims,
+  title=expression(paste(rho[1], " = 0.1", ", r = 0.8"))
+) + geom_hline(yintercept=0, linetype='dashed')
+
+# Combine grid plots
+mylegend <- g_legend(p1)
+title <- expression(paste("Bias for ", hat(r)))
+p_bias_CAC <- make_1x2_multiplot(p1, p2, mylegend, title)
+ggsave("plots/bias_CAC.jpg", p_bias_CAC, width=9, height=4, units="in", dpi=800)
+ggsave("plots/bias_CAC.pdf", p_bias_CAC, width=9, height=4, units="in", dpi=600)
+
+
 ## Plot MSE for theta
 
 # Determine common y-axis limits
@@ -373,10 +415,6 @@ MSEdf_WPICC5_CAC80_BPICC <- MSEdfHG %>%
   filter(rho1==0.05 & r==0.8 & parameters=='BPICC')
 MSEdf_WPICC10_CAC80_BPICC <- MSEdfHG %>%
   filter(rho1==0.1 & r==0.8 & parameters=='BPICC')
-MSEdf_WPICC5_CAC100_BPICC <- MSEdfHH %>%
-  filter(rho1==0.05 & r==1.0 & parameters=='BPICC')
-MSEdf_WPICC10_CAC100_BPICC <- MSEdfHH %>%
-  filter(rho1==0.1 & r==1.0 & parameters=='BPICC')
 
 # Generate grid plots
 p1 <- plotminigrid(
@@ -396,6 +434,47 @@ title <- expression(paste("MSE for ", hat(rho[2])))
 p_MSE_BPICC <- make_1x2_multiplot(p1, p2, mylegend, title)
 ggsave("plots/MSE_BPICC.jpg", p_MSE_BPICC, width=9, height=4, units="in", dpi=800)
 ggsave("plots/MSE_BPICC.pdf", p_MSE_BPICC, width=9, height=4, units="in", dpi=600)
+
+
+## Plot MSE for CAC
+
+# Determine common y-axis limits
+maxMSEHG <- signif(
+  MSEdfHG %>%
+    filter(parameters=='CAC') %>%
+    summarise(max(value)),
+  2)
+maxMSEHH <- signif(
+  MSEdfHH %>%
+    filter(parameters=='CAC') %>%
+    summarise(max(value)),
+  2)
+ylims <- c(0, max(maxMSEHG, maxMSEHH))
+
+# Get separate results blocks for plotting
+MSEdf_WPICC5_CAC80_CAC <- MSEdfHG %>%
+  filter(rho1==0.05 & r==0.8 & parameters=='CAC')
+MSEdf_WPICC10_CAC80_CAC <- MSEdfHG %>%
+  filter(rho1==0.1 & r==0.8 & parameters=='CAC')
+
+# Generate grid plots
+p1 <- plotminigrid(
+  MSEdf_WPICC5_CAC80_CAC,
+  ylims=ylims,
+  title=expression(paste(rho[1], " = 0.05", ", r = 0.8"))
+)
+p2 <- plotminigrid(
+  MSEdf_WPICC10_CAC80_CAC,
+  ylims=ylims,
+  title=expression(paste(rho[1], " = 0.1", ", r = 0.8"))
+)
+
+# Combine grid plots
+mylegend <- g_legend(p1)
+title <- expression(paste("MSE for ", hat(r)))
+p_MSE_CAC <- make_1x2_multiplot(p1, p2, mylegend, title)
+ggsave("plots/MSE_CAC.jpg", p_MSE_CAC, width=9, height=4, units="in", dpi=800)
+ggsave("plots/MSE_CAC.pdf", p_MSE_CAC, width=9, height=4, units="in", dpi=600)
 
 
 ## Plot 95% confidence/credible interval coverage for theta
@@ -638,3 +717,58 @@ title <- expression(paste("Empirical and model-based SE for ", hat(theta)))
 p_bothSE_theta <- make_2x2_multiplot(p1, p2, p3, p4, mylegend, title)
 ggsave("plots/bothSE_theta.jpg", p_bothSE_theta, width=9, height=7, units="in", dpi=800)
 ggsave("plots/bothSE_theta.pdf", p_bothSE_theta, width=9, height=7, units="in", dpi=600)
+
+
+## Plot relative % error in model-based SE for theta
+
+# Determine common y-axis limits
+maxpcterrmodSEHG <- signif(
+  pcterrmodSEdfHG %>%
+    filter(parameters=='theta') %>%
+    summarise(max(value)),
+  2)
+maxpcterrmodSEHH <- signif(
+  pcterrmodSEdfHH %>%
+    filter(parameters=='theta') %>%
+    summarise(max(value)),
+  2)
+ylims <- c(0, max(maxpcterrmodSEHG, maxpcterrmodSEHH))
+
+# Get separate results blocks for plotting
+pcterrmodSEdf_WPICC5_CAC80_theta <- pcterrmodSEdfHG %>%
+  filter(rho1==0.05 & r==0.8 & parameters=='theta')
+pcterrmodSEdf_WPICC10_CAC80_theta <- pcterrmodSEdfHG %>%
+  filter(rho1==0.1 & r==0.8 & parameters=='theta')
+pcterrmodSEdf_WPICC5_CAC100_theta <- pcterrmodSEdfHH %>%
+  filter(rho1==0.05 & r==1.0 & parameters=='theta')
+pcterrmodSEdf_WPICC10_CAC100_theta <- pcterrmodSEdfHH %>%
+  filter(rho1==0.1 & r==1.0 & parameters=='theta')
+
+# Generate grid plots
+p1 <- plotminigrid(
+  pcterrmodSEdf_WPICC5_CAC80_theta,
+  ylims=ylims,
+  title=expression(paste(rho[1], " = 0.05", ", r = 0.8"))
+)
+p2 <- plotminigrid(
+  pcterrmodSEdf_WPICC10_CAC80_theta,
+  ylims=ylims,
+  title=expression(paste(rho[1], " = 0.1", ", r = 0.8"))
+)
+p3 <- plotminigrid(
+  pcterrmodSEdf_WPICC5_CAC100_theta,
+  ylims=ylims,
+  title=expression(paste(rho[1], " = 0.05", ", r = 1.0"))
+)
+p4 <- plotminigrid(
+  pcterrmodSEdf_WPICC10_CAC100_theta,
+  ylims=ylims,
+  title=expression(paste(rho[1], " = 0.1", ", r = 1.0"))
+)
+
+# Combine grid plots
+mylegend <- g_legend(p1)
+title <- expression(paste("Relative % error in model-based SE for ", hat(theta)))
+p_pcterrmodSE_theta <- make_2x2_multiplot(p1, p2, p3, p4, mylegend, title)
+ggsave("plots/pcterrmodSE_theta.jpg", p_pcterrmodSE_theta, width=9, height=7, units="in", dpi=800)
+ggsave("plots/pcterrmodSE_theta.pdf", p_pcterrmodSE_theta, width=9, height=7, units="in", dpi=600)
