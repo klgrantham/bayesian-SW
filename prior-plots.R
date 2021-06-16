bcbe7d36d37ffd63b16b35d20db894805caba9bf
Kelsey L Grantham <kelsey.grantham@monash.edu>
2021-06-17 09:52:40 +1000

    Combine prior plots into a single figure and implied distributions into another figure

--- prior-plots.R
+++ prior-plots.R
@@ -41,7 +41,7 @@ sig2cp_implied <- function(sig2e, WPICC, sig2c){
 
 plot_prior <- function(df) {
   p <- ggplot(df) +
-        geom_area(aes(x=x, y=y), color="darkorange", fill="darkorange", alpha=0.6) +
+        geom_line(aes(x=x, y=y), color="darkorange", size=1) +
         ylab("") +
         theme_bw() +
         theme(plot.title = element_text(hjust = 0.5, size=20),
@@ -51,7 +51,7 @@ plot_prior <- function(df) {
 
 plot_dens <- function(densvals) {
   p <- ggplot() +
-        geom_density(aes(x=densvals), fill="slateblue", alpha=0.6) +
+        geom_density(aes(x=densvals), color="slateblue", size=1) +
         ylab("") +
         theme_bw() +
         theme(plot.title = element_text(hjust = 0.5, size=20),
@@ -62,36 +62,45 @@ plot_dens <- function(densvals) {
 # Specify prior distributions for plotting
 theta_prior <- data.frame(x=seq(-400, 400, length.out=100))
 theta_prior$y <- dnorm(theta_prior$x, 0, 100)
-plot_prior(theta_prior) +
-  geom_vline(aes(xintercept=0), color="steelblue", size=1) +
+p_theta <- plot_prior(theta_prior) +
+  geom_vline(aes(xintercept=0), color="steelblue", linetype="dashed", size=1) +
   xlab(expression(theta)) +
-  ggtitle(expression(paste(theta, " ~ N(0, 10000)")))
-ggsave("plots/prior_theta.jpg", width=6, height=6, units="in", dpi=600)
+  ggtitle(expression(paste(theta, " ~ N(0, ", 10^4, ")")))
+#ggsave("plots/prior_theta.jpg", width=6, height=6, units="in", dpi=600)
+
+betaj_prior <- data.frame(x=seq(-400, 400, length.out=100))
+betaj_prior$y <- dnorm(theta_prior$x, 0, 100)
+p_betaj <- plot_prior(betaj_prior) +
+  xlab(expression(beta[j])) +
+  ggtitle(expression(paste(beta[j], " ~ N(0, ", 10^4, ")")))
 
 WPICC_prior <- data.frame(x=seq(0, 1, length.out=100))
 WPICC_prior$y <- dbeta(WPICC_prior$x, 1.5, 10.5)
-plot_prior(WPICC_prior) +
-  geom_vline(aes(xintercept=0.05), color="steelblue", size=1) +
-  geom_vline(aes(xintercept=0.1), color="darkseagreen", size=1) +
+p_WPICC <- plot_prior(WPICC_prior) +
+  geom_vline(aes(xintercept=0.05), color="steelblue", linetype="dashed", size=1) +
+  geom_vline(aes(xintercept=0.1), color="darkseagreen", linetype="dashed", size=1) +
   xlab(expression(rho[1])) +
   ggtitle(expression(paste(rho[1], " ~ Beta(1.5, 10.5)")))
-ggsave("plots/prior_WPICC.jpg", width=6, height=6, units="in", dpi=600)
+#ggsave("plots/prior_WPICC.jpg", width=6, height=6, units="in", dpi=600)
 
 CAC_prior <- data.frame(x=seq(0, 1, length.out=100))
 CAC_prior$y <- dbeta(CAC_prior$x, 5, 2)
-plot_prior(CAC_prior) +
-  geom_vline(aes(xintercept=0.8), color="steelblue", size=1) +
+p_CAC <- plot_prior(CAC_prior) +
+  geom_vline(aes(xintercept=0.8), color="steelblue", linetype="dashed", size=1) +
   xlab(expression(r)) +
   ggtitle(expression(paste(r, " ~ Beta(5, 2)")))
-ggsave("plots/prior_CAC.jpg", width=6, height=6, units="in", dpi=600)
+#ggsave("plots/prior_CAC.jpg", width=6, height=6, units="in", dpi=600)
 
 sig2e_prior <- data.frame(x=seq(0, 50, length.out=100))
 sig2e_prior$y <- dhcauchy(sig2e_prior$x, 1)
-plot_prior(sig2e_prior) +
-  geom_vline(aes(xintercept=1.0), color="steelblue", size=1) +
+p_sig2e <- plot_prior(sig2e_prior) +
+  geom_vline(aes(xintercept=1.0), color="steelblue", linetype="dashed", size=1) +
   xlab(expression(sigma[e]^2)) +
   ggtitle(expression(paste(sigma[e]^2, " ~ Half-Cauchy(0, 1)")))
-ggsave("plots/prior_sig2e.jpg", width=6, height=6, units="in", dpi=600)
+#ggsave("plots/prior_sig2e.jpg", width=6, height=6, units="in", dpi=600)
+
+p_all <- grid.arrange(arrangeGrob(p_theta, p_betaj, p_sig2e, p_WPICC, p_CAC, nrow=2))
+ggsave("plots/allpriors.jpg", p_all, width=12, height=7, units="in", dpi=400)
 
 # Implied distributions
 sig2e_vals <- rhcauchy(1e5, 1)
@@ -123,18 +132,17 @@ plot_dens(BPICC_prior) +
   xlim(c(0, 1))
 ggsave("plots/priors_BPICC_implied.jpg", width=6, height=6, units="in", dpi=600)
 
-plot_dens(sig2c_prior) +
-  geom_vline(aes(xintercept=sig_sq_c1), color="steelblue", size=1) +
-  geom_vline(aes(xintercept=sig_sq_c2), color="darkseagreen", size=1) +
+p_sig2c <- plot_dens(sig2c_prior) +
   xlab(expression(sigma[C]^2)) +
   ggtitle(expression(paste("Implied distribution of ", sigma[C]^2))) +
   xlim(c(0, 5))
 ggsave("plots/priors_sig2c_implied.jpg", width=6, height=6, units="in", dpi=600)
 
-plot_dens(sig2cp_prior) +
-  geom_vline(aes(xintercept=sig_sq_cp1), color="steelblue", size=1) +
-  geom_vline(aes(xintercept=sig_sq_cp2), color="darkseagreen", size=1) +
+p_sig2cp <- plot_dens(sig2cp_prior) +
   xlab(expression(sigma[CP]^2)) +
   ggtitle(expression(paste("Implied distribution of ", sigma[CP]^2))) +
   xlim(c(0, 5))
 ggsave("plots/priors_sig2cp_implied.jpg", width=6, height=6, units="in", dpi=600)
+
+p_vars <- grid.arrange(arrangeGrob(p_sig2c, p_sig2cp, nrow=1))
+ggsave("plots/impliedpriors.jpg", p_vars, width=8, height=4, units="in", dpi=400)
