a758a7d162dd4f20ef29d1526b015352b0259ffc
Kelsey L Grantham <kelsey.grantham@monash.edu>
2021-06-17 10:05:31 +1000

    Reorganised in accordance with paper

--- results_tables.R
+++ results_tables.R
@@ -24,23 +24,6 @@ fmtMCSE <- function(x, digits){
   sf
 }
 
-create_Latex_table_multpars <- function(df, maindigits=1, MCSEdigits=1, pars, parnames) {
-  toLatex(
-    tabular(
-      RowFactor(rho1, name="$\\rho_1$", spacing=1)
-      * RowFactor(Tp, name="T", spacing=1)
-      * RowFactor(m, spacing=1)
-      * Factor(S)
-      ~ Heading()*RowFactor(factor(parameters, levels=pars), levelnames=parnames)
-      * Heading()*RowFactor(Method)
-      * Heading()*(Format(fmtmain(digits=maindigits))*value +
-                   Format(fmtMCSE(digits=MCSEdigits))*MCSE)
-      * Heading()*identity,
-      data=df
-    )
-  )
-}
-
 create_Latex_table <- function(df, maindigits=1, MCSEdigits=1) {
   toLatex(
     tabular(
@@ -58,30 +41,48 @@ create_Latex_table <- function(df, maindigits=1, MCSEdigits=1) {
   )
 }
 
+# Table 3: Calculate percentage of valid replicates
+allreprows <- allreprows %>%
+  mutate(Bayesian=MCMCreps/1e3*100, REML=REMLreps/1e3*100)
+toLatex(
+  tabular(
+    Factor(rho1, name="$\\rho_1$")
+    * Factor(Tp, name="T")
+    * Factor(m)
+    * Factor(S)
+    ~ Factor(r, name="$r$")
+    * (Format(fmtmain(digits=3))*Bayesian +
+         Format(fmtmain(digits=3))*REML)
+    * Heading()*identity,
+    data=allreprows
+  )
+)
+
+# Table 4: Bias for theta
 biasdftheta <- rbind(biasdfHH %>% filter(parameters=='theta'),
                      biasdfHG %>% filter(parameters=='theta'))
-create_Latex_table(biasdftheta, 2, 1)
-
 biasdftheta_scaled <- biasdftheta %>% 
   rename(value_unscaled=value, MCSE_unscaled=MCSE) %>%
   mutate(value=value_unscaled*1e4, MCSE=MCSE_unscaled*1e4)
 create_Latex_table(biasdftheta_scaled, 1, 1)
 
+# Table 5: MSE for theta
 MSEdftheta <- rbind(MSEdfHH %>% filter(parameters=='theta'),
                     MSEdfHG %>% filter(parameters=='theta'))
-create_Latex_table(MSEdftheta, 2, 1)
-
 MSEdftheta_scaled <- MSEdftheta %>%
   rename(value_unscaled=value, MCSE_unscaled=MCSE) %>%
   mutate(value=value_unscaled*1e4, MCSE=MCSE_unscaled*1e4)
 create_Latex_table(MSEdftheta_scaled, 1, 1)
 
+# Table 6: Confidence/credible interval coverage for theta
 covdftheta <- rbind(covdfHH, covdfHG)
-create_Latex_table(covdftheta, 2, 1)
+create_Latex_table(covdftheta, 3, 1)
 
+# Table 7: Relative % error in model-based SE for theta
 pcterrmodSEdftheta <- rbind(pcterrmodSEdfHH, pcterrmodSEdfHG)
 create_Latex_table(pcterrmodSEdftheta, 1, 2)
 
+# Table 8: Confidence/credible interval width for theta
 intlendftheta <- rbind(intlendfHH, intlendfHG)
 toLatex(
   tabular(
@@ -97,78 +98,32 @@ toLatex(
   )
 )
 
+# Table 9: Bias for WPICC
 biasdfWPICC <- rbind(biasdfHH %>% filter(parameters=='WPICC'),
                      biasdfHG %>% filter(parameters=='WPICC'))
-create_Latex_table(biasdfWPICC, 2, 1)
-
 biasdfWPICC_scaled <- biasdfWPICC %>%
   rename(value_unscaled=value, MCSE_unscaled=MCSE) %>%
   mutate(value=value_unscaled*1e4, MCSE=MCSE_unscaled*1e4)
 create_Latex_table(biasdfWPICC_scaled, 1, 1)
 
+# Table 10: MSE for WPICC
 MSEdfWPICC <- rbind(MSEdfHH %>% filter(parameters=='WPICC'),
                     MSEdfHG %>% filter(parameters=='WPICC'))
-create_Latex_table(MSEdfWPICC, 2, 1)
-
 MSEdfWPICC_scaled <- MSEdfWPICC %>%
   rename(value_unscaled=value, MCSE_unscaled=MCSE) %>%
   mutate(value=value_unscaled*1e4, MCSE=MCSE_unscaled*1e4)
 create_Latex_table(MSEdfWPICC_scaled, 1, 1)
 
+# Table 11: Bias for CAC
 biasdfCAC <- biasdfHG %>% filter(parameters=='CAC')
-create_Latex_table(biasdfCAC, 2, 1)
-
 biasdfCAC_scaled <- biasdfCAC %>%
   rename(value_unscaled=value, MCSE_unscaled=MCSE) %>%
   mutate(value=value_unscaled*1e4, MCSE=MCSE_unscaled*1e4)
 create_Latex_table(biasdfCAC_scaled, 1, 1)
 
+# Table 12: MSE for CAC
 MSEdfCAC <- MSEdfHG %>% filter(parameters=='CAC')
-create_Latex_table(MSEdfCAC, 2, 1)
-
 MSEdfCAC_scaled <- MSEdfCAC %>%
   rename(value_unscaled=value, MCSE_unscaled=MCSE) %>%
   mutate(value=value_unscaled*1e4, MCSE=MCSE_unscaled*1e4)
 create_Latex_table(MSEdfCAC_scaled, 1, 1)
-
-
-create_Latex_table_multpars(biasdfHH, maindigits=2, MCSEdigits=1,
-                   pars=c('theta', 'WPICC'), parnames=c("$\\theta$", "$\\rho_1$"))
-create_Latex_table_multpars(biasdfHG, maindigits=2, MCSEdigits=1,
-                   pars=c('theta', 'WPICC', 'CAC', 'BPICC'),
-                   parnames=c("$\\theta$", "$\\rho_1$", "r", "$\\rho_2$"))
-create_Latex_table_multpars(MSEdfHH, maindigits=2, MCSEdigits=1,
-                   pars=c('theta', 'WPICC'), parnames=c("$\\theta$", "$\\rho_1$"))
-
-# Calculate success rates
-allreprows <- allreprows %>%
-  mutate(Bayesian=MCMCreps/1e3*100, REML=REMLreps/1e3*100)
-
-toLatex(
-  tabular(
-    Factor(r, name="$r$")
-    * Factor(rho1, name="$\\rho_1$")
-    * Factor(Tp, name="T")
-    * Factor(m)
-    * Factor(S)
-    ~ (Format(fmtmain(digits=3))*Bayesian +
-      Format(fmtmain(digits=3))*REML)
-    * Heading()*identity,
-    data=allreprows
-  )
-)
-
-# Widen by putting r values in different columns
-toLatex(
-  tabular(
-    Factor(rho1, name="$\\rho_1$")
-    * Factor(Tp, name="T")
-    * Factor(m)
-    * Factor(S)
-    ~ Factor(r, name="$r$")
-    * (Format(fmtmain(digits=3))*Bayesian +
-       Format(fmtmain(digits=3))*REML)
-    * Heading()*identity,
-    data=allreprows
-  )
-)
